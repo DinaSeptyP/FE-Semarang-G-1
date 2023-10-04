bf0be0ce3ee688a8a6d25358945e786ca41562ef
Dina Septy Puspayani <dinaseptypuspayani87@gmail.com>
2023-10-04 11:26:27 +0700

    first commit

--- public/js/script.js
+++ public/js/script.js
@@ -35,6 +35,24 @@ btn.addEventListener('click', () => {
       icon: 'success',
     }).then((result) => {
       if (result.isConfirmed) {
+        fetch('https://be-semarang-g-1-production.up.railway.app/api/submit-form', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name, email, message }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.success) {
+              console.log('Data successfully sent to the server.');
+            } else {
+              console.error('Error sending data to the server:', data.error);
+            }
+          })
+          .catch((error) => {
+            console.error('Error sending data to the server:', error);
+          });
           window.location.href = "https://dinaseptyp.github.io/FE-Semarang-G-1.github.io/";
       }
   });
