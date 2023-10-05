ea8e1f7f2ce9bca1e005d7c5a9f34da2ea430399
Dina Septy Puspayani <dinaseptypuspayani87@gmail.com>
2023-10-05 10:07:31 +0700

    first commit

--- public/js/login.js
+++ public/js/login.js
@@ -38,29 +38,24 @@ function submitLogin(event) {
   
   document.addEventListener("DOMContentLoaded", function () {
     const token = localStorage.getItem('token'); // token from Local storage
-    let requestSent = false;
-
     if (!token) {
         // Token not found, balik ke halaman login
         window.location.href = 'https://dinaseptyp.github.io/FE-Semarang-G-1.github.io/admin/login.html';
     } else {
         // Kirim permintaan GET ke server dengan token
-        if (!requestSent) {
-            fetch(`${hostUrl}/api/admin/data`, {
-                method: 'GET',
-                headers: {
-                    'authorization': `Bearer ${token}` // Kirim token dalam header "Authorization"
-                }
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                requestSent = true; // Set status permintaan sudah terkirim setelah berhasil
-            })
-            .catch(error => {
-                console.error('Error fetching data:', error);
-            });
-        }
+        fetch(`${hostUrl}/api/admin/data`, {
+            method: 'GET',
+            headers: {
+                'authorization': `Bearer ${token}` // Kirim token dalam header "Authorization"
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
     }
-});
+  });
